a477edf65e29732d25b64999f1364c09eab6567f
dityara17 <arizki717@gmail.com>
2020-12-09 19:55:17 +0700

    init

---
+++ app/Model/Users.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Users extends Model
+{
+    protected $table = "users";
+
+
+    public function getUser($id)
+    {
+        return "user";
+    }
+}
